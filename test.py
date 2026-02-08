3919333ae9ac1882b5e028e20ba0bc3a23f1340a
Nevexs <romain.zepp@gmail.com>
2026-02-08 19:27:54 +0100

    Commit VERY different (testing merge conflict)

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Saleté")
+print("AAAAAAAAAAAAAAAAH")
+print("BBBBBBBBBBBBBBBBH")
+print("CCCCCCCCCCCCCCCCH")
+
+from colorama import Fore
+
+def scream():
+    return Fore.YELLOW + "Saleté " * 10**6
+
+print(scream())

6834ea8b6228f0abdca1c3f1bb100daa0b56df3d
Nevexs <romain.zepp@gmail.com>
2026-02-08 14:58:15 +0100

    File were are our Data Structures choicess

---
+++ data/Structures de Données.py
@@ -0,0 +1,50 @@
+game_rules = {
+    "map-size": (39, 40),
+    "turns-to-win": 25,
+    "spawn": [(20, 3), (20, 38)],
+    "spur": [
+        (20, 38),
+        (20, 39),
+        (21, 38),
+        (21, 39)
+    ]
+}
+
+creatures = {
+    "bear": {
+        "position": (10, 10),
+        "health": 20,
+        "damage": 5,
+        "range": 3
+    },
+    "wolf": {
+        "position": (15, 10),
+        "health": 10,
+        "damage": 3,
+        "range": 2
+    }
+}
+
+heroes = {
+    "player-1": {
+        "hero-1": {
+            "name": "Gigachad",
+            "class": "mage",
+            "position": [15, 10],
+            "health": 10,
+            "damage": 3
+        },
+        "hero-2": {
+            ...
+        },
+        "hero-3": ...,
+        "hero-4": ...
+    },
+
+    "player-2": {
+        "hero-1": ...,
+        "hero-2": ...,
+        "hero-3": ...,
+        "hero-4": ...,
+    }
+}

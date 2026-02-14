aad3cd56eba4e0ba6b63bc5c80c01ef076ec62ec
MissLye <lisettedevillers3@gmail.com>
2026-02-14 14:45:37 +0100

    Update specification

--- data/commands.py
+++ data/commands.py
@@ -4,9 +4,10 @@
 
 #-------------Structure de donnée------------------#
 
+actions = { "shifting" : [] , "attack" : [] , "special_attack" : []}
 
-#-------------------Fonctions----------------------#
-
+#-----------------------------Fonctions--------------------------------#
+#--------------Gestion de commandes----------------#
 
 
 def get_commande_separed (orders : str) -> list:
@@ -19,6 +20,10 @@ def get_commande_separed (orders : str) -> list:
     Return:
     -------
     list: Every element is an order witch is a list, the first element is hero's name or creature's name and the second is the action or the type of hero
+
+    Version:
+    --------
+    Lisette DEVILLERS (v.1 14/02/2026)
     """
     orders_list = []
     simple_list_orders = orders.split () # ordres sous cette forme : ["albert:*32-4" , "denver:@34-4" , "daniel:energise"]
@@ -28,28 +33,82 @@ def get_commande_separed (orders : str) -> list:
 
 # On a donc une liste comme ceci: [['albert', '*32-4'], ['denver', '@34-4'], ['daniel', 'energise']]
 
-actions = { "shifting" : [] , "attack" : [] , "special_attack" : []}
 
-def get_in_details_order (order : list):
+def get_in_dict_orders (orders : list) -> dict:
     """
-    Add in action dictionnary the order
+    Create a dictionnary with for differents actions, a list wich contain all action's orders
     
     Parameters:
-    order(list): order is a list, the first element is hero's name or creature's name and the second is the action
+    -----------
+    orders(list): a list wich contain all orders. On order is a list, the first element is hero's name or creature's name and the second is the action
+
+    Return:
+    -------
+    dict: one key is for the list of orders of one action's type.
+
+    Version:
+    --------
+    Lisette DEVILLERS (v.1 14/02/2026)
     """
+    
+
     # dico sous cette forme: action: { shifting : [ (name , (x , y)) , (name , (x , y)) ....] , attack : [(name , (x , y)) , (name , (x , y)) .... ] , special_attack : [(name , type) , (name , type) ....]}
 
 
+#--------------Aplition de commandes---------------#
 
-
-def execute_order (order : dict): 
+def execute_orders (orders : dict): 
     """
-    Execute the order
+    Execute the orders
 
     Parameters:
-    order (dict): One order like this: name_chara : {"action" : type , "target" : (x : y)}
+    -----------
+    orders (dict): a dictionnary wich associate for differents actions, a list wich contain all action's orders
 
     PS: Use tree other function: shifting, attack, special attack
+
+    Version:
+    --------
+    Lisette DEVILLERS (v.1 14/02/2026)
+    """
+
+def shifting (order : list ):
+    """
+    Move a character
+    
+    Parameter:
+    ----------
+    order (list): the first element is hero's name or creature's name and the second is the coordinates where the character want to go
+
+    Version:
+    --------
+    Lisette DEVILLERS (v.1 14/02/2026)
+    """
+
+def attack (order : list):
+    """
+    Attack a character
+
+    Parameter:
+    ----------
+    order (list): the first element is hero's name or creature's name who want to attack and the second is the victime's coordinates
+
+    Version:
+    --------
+    Lisette DEVILLERS (v.1 14/02/2026)
+    """
+
+def special_attack (order : list):
+    """
+    Execute the special attack
+    
+    Parameter:
+    ----------
+    order (list): the first element is hero's name and the second is attack's name
+
+    Version:
+    --------
+    Lisette DEVILLERS (v.1 14/02/2026)
     """
 
 
